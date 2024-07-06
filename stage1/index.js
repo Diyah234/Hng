38da5652781af8b95e37737dcc8a5344c6ae56ce
Abudu Rodiyyah <rodiyahabudu@gmail.com>
2024-07-06 22:47:48 +0100

    update

--- stage1/index.js
+++ stage1/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/hello', async (req, res) => {
-  
-  
-
   try {
+    // Retrieve client IP address from headers or socket
+    const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
     // Fetch location data based on the client's IP address
-    const locationResponse = await axios.get("https://api.ipfind.com/me?auth=0e68b218-c41c-4df7-a5ed-6cffa0534985");
-    const location = locationResponse.data.city;
-    const clientIp = locationResponse.data.ip_address
+    const locationResponse = await axios.get(`https://api.ipfind.com/?ip=${clientIp}&auth=0e68b218-c41c-4df7-a5ed-6cffa0534985`);
+    const location = locationResponse.data.city || 'Unknown City';
 
     // Fetch weather data based on the location
-    const apiKey = "3c32c154af15eb3424aae4d34506bb7c";
+    const apiKey = process.env.OPENWEATHERMAP_API_KEY || "3c32c154af15eb3424aae4d34506bb7c"; // Use environment variable or fallback to default
     const units = "metric";
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=${units}`;
 
@@ -37,6 +37,6 @@ app.get('/api/hello', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running on port 3000`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
